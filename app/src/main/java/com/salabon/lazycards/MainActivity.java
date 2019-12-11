c5f092d562f508d1109e967d9d31eef767d1fda4
Mick <salabon77mk@gmail.com>
2019-12-11 15:37:11 -0500

    First commit

---
+++ app/src/main/java/com/salabon/lazycards/MainActivity.java
@@ -0,0 +1,14 @@
+package com.salabon.lazycards;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class MainActivity extends SingleAbstractActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/salabon/lazycards/MainFragment.java
@@ -0,0 +1,6 @@
+package com.salabon.lazycards;
+
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+}
