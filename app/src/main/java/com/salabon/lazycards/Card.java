0a888a8e9105cec2d5cc7540294eb17a61a6249c
Mick <salabon77mk@gmail.com>
2020-02-26 12:57:24 -0500

    Add card model

---
+++ app/src/main/java/com/salabon/lazycards/Card.java
@@ -0,0 +1,76 @@
+package com.salabon.lazycards;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Card {
+    private String mVocabWord;
+    private String mBackofCard;
+    private String mDeck;
+    private String mTags;
+    private String mSelectedOptions;
+
+    private int mApi; // the api used
+    
+    public String getVocabWord() {
+        return mVocabWord;
+    }
+
+    public void setVocabWord(String vocabWord) {
+        mVocabWord = vocabWord;
+    }
+
+    public String getBackofCard() {
+        return mBackofCard;
+    }
+
+    public void setBackofCard(String backofCard) {
+        mBackofCard = backofCard;
+    }
+
+    public String getDeck() {
+        return mDeck;
+    }
+
+    public void setDeck(String deck) {
+        mDeck = deck;
+    }
+
+    public String getTags() {
+        return mTags;
+    }
+
+    public void setTags(String tags) {
+        mTags = tags;
+    }
+
+    public int getApi() {
+        return mApi;
+    }
+
+    public void setApi(int api) {
+        mApi = api;
+    }
+
+    public List<String> getSelectedOptionsAsList() {
+        return Arrays.asList(mSelectedOptions.split(","));
+    }
+
+    // for interacting with database
+    public String getSelectOptionsAsString(){
+        return mSelectedOptions;
+    }
+
+    public void setSelectedOptions(List<String> selectedOptions) {
+        StringBuilder build = new StringBuilder();
+        for(String op : selectedOptions){
+            build.append(op).append(',');
+        }
+        mSelectedOptions = build.toString();
+
+        // remove the last comma
+        if(mSelectedOptions.length() > 0) {
+            mSelectedOptions = mSelectedOptions.substring(0, mSelectedOptions.length() - 1);
+        }
+    }
+}
